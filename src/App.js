8c4f1ae411236855723e66a68d885864a50ce2bd
Luis Leon <114199533+Louie218@users.noreply.github.com>
2023-04-14 01:03:37 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
